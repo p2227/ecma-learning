5cef2e744eb946aa9f601c8709e06e55530d7e0a
kuang <p2227@hotmail.com>
2016-03-30 10:50:19 +0800

    Update promise.spec.js

--- test/promise.spec.js
+++ test/promise.spec.js
@@ -1,6 +1,7 @@
-import { expect } from 'chai';
+var sinon  = require('sinon');
+var expect = require('chai').expect;
 
-describe('Promise基本用法',function(){
+describe('Promise的基本用法',function(){
 	it('前一个的返回值应该在后一个的then方法参数中',function(done){
 		var q = new Promise(function(resolve,reject){
 			setTimeout(function(){
@@ -37,47 +38,41 @@ describe('Promise基本用法',function(){
 	})
 
 	it('reject，将会跳过then的第一个函数的执行',function(done){
-		var result = 0;
 		var q = new Promise(function(resolve,reject){
 			setTimeout(function(){
 				reject()
 			},1);
 		});
 
-		q.then(function(args){
-			result++;
-		},function(){
-			result++;
-			expect(result).to.be.equal(1);
-			done();
-		})
+		const callbackThen = sinon.spy();
+        const callbackCatch = sinon.spy();
+        q.then(callbackThen,callbackCatch).then(function(){
+        	expect(callbackThen.called).to.not.be.ok;
+        	expect(callbackCatch.calledOnce).to.be.ok;
+        	done();
+        });
 	})
 
 	it('抛出错误，将会跳过then的第一个函数的执行',function(done){
-		var result = 0;
 		var q = new Promise(function(resolve,reject){
 			throw new Error();
 		});
 
-		q.then(function(args){
-			result++;
-		},function(){
-			result++;
-			expect(result).to.be.equal(1);
-			done();
-		})
+		const callbackThen = sinon.spy();
+        const callbackCatch = sinon.spy();
+        q.then(callbackThen,callbackCatch).then(function(){
+        	expect(callbackThen.called).to.not.be.ok;
+        	expect(callbackCatch.calledOnce).to.be.ok;
+        	done();
+        });
 	});
 
 	it('抛出错误之后仍然可以继续Promise链式操作',function(done){
-		var result = 0;
 		var q = new Promise(function(resolve,reject){
 			throw new Error();
 		});
 
-		q.then(function(args){
-			result++;
-		}).catch(function(){
-			result++;
+		q.catch(function(){
 			return new Promise(function(resolve){
 				resolve('foo')
 			});
@@ -88,26 +83,21 @@ describe('Promise基本用法',function(){
 	})
 
 	it('reject或者抛出错误，忽略中间所有（只有一个参数）的then',function(done){
-		var result = 0;
 		var q = new Promise(function(resolve,reject){
 			reject('foo');
 		});
 
-		q.then(function(args){
-			result++;
-		}).then(function(args){
-			result++;
-		}).then(function(args){
-			result++;
-		}).then(function(args){
-			result++;
-		}).then(function(args){
-			result++;
-		}).then(function(args){
-			result++;
-		}).catch(function(args){
-			result++;
-			expect(result).to.be.equal(1);
+		const callback = sinon.spy();
+
+		q.then(callback)
+		 .then(callback)
+		 .then(callback)
+		 .then(callback)
+		 .then(callback)
+		 .then(callback)
+		 .catch(function(args){
+		 	callback();
+			expect(callback.callCount).to.equal(1);
 			expect(args).to.be.equal('foo');
 			done();
 		});
